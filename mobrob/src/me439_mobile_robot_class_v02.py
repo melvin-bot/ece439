7481f7148cc59313894e4ca75d99ab5bb54758ab
SpookySquidward <willrkramer@gmail.com>
2023-10-18 15:59:33 -0500

    Calculated vecangle and reformatted comments

--- mobrob/src/me439_mobile_robot_class_v02.py
+++ mobrob/src/me439_mobile_robot_class_v02.py
@@ -229,17 +229,17 @@ class robot:
         dist = np.sqrt((xf-x0) ** 2 + (yf-y0) ** 2)     # distance from [x0,y0] to [xf,yf], a Scalar
         
         # Code to determine how a circular path of Radius R can get from start to finish of this path segment.  
-        if dist > 2.0*np.abs(R):            # If the endpoint is farther away than the diameter of the circle, an arc cannot get there and you must draw a line instead. 
-            specs = self.specify_line(x0,y0,xf,yf)  # Use the "self.specify_line(...)" function above for this case! 
-        else:                       # Otherwise find an arc. 
-            arcangle = 2.0*np.arcsin((dist/2.0)/R )     # Smallest arc angle is swept if going the "short way" around the circle. The distance from start to finish is the "chord length" and relates to the included angle as shown. 
+        if dist > 2.0*np.abs(R):                                # If the endpoint is farther away than the diameter of the circle, an arc cannot get there and you must draw a line instead. 
+            specs = self.specify_line(x0,y0,xf,yf)              # Use the "self.specify_line(...)" function above for this case! 
+        else:                                                   # Otherwise find an arc. 
+            arcangle = 2.0*np.arcsin((dist/2.0)/R )             # Smallest arc angle is swept if going the "short way" around the circle. The distance from start to finish is the "chord length" and relates to the included angle as shown. 
             if way.lower() == 'long':
-                arcangle = 2*np.pi*np.sign(arcangle) - arcangle     # arc angle swept if going the "long way": a full circle is 2*pi, so the long way is 2*pi minus the short way. Note the signs. 
-            vecangle = 0.0       # Vector angle: angle of the vector directly from the start point to the end point.
-            initangle = vecangle - arcangle/2.0         # Initial tangent angle of the arc to be followed. Deviates from the direct straignt line by half the included angle of the arc. 
-            initangle = fix_angle_pi_to_neg_pi(initangle)   # Could have gone past pi with the addition. Unwrap to make sure it's within +/- pi. 
-            arclength = arcangle * R                      # arc length is also needed. Enter it based on Radius and Angle. 
-            specs = [x0,y0,initangle, R, arclength]
+                arcangle = 2*np.pi*np.sign(arcangle) - arcangle # arc angle swept if going the "long way": a full circle is 2*pi, so the long way is 2*pi minus the short way. Note the signs. 
+            vecangle = np.arctan(vec[0] / vec[1])               # Vector angle: angle of the vector directly from the start point to the end point.
+            initangle = vecangle - arcangle/2.0                 # Initial tangent angle of the arc to be followed. Deviates from the direct straignt line by half the included angle of the arc. 
+            initangle = fix_angle_pi_to_neg_pi(initangle)       # Could have gone past pi with the addition. Unwrap to make sure it's within +/- pi. 
+            arclength = arcangle * R                            # arc length is also needed. Enter it based on Radius and Angle. 
+            specs = [x0, y0, initangle, R, arclength]
 ####    CODE END        
         
         return specs	

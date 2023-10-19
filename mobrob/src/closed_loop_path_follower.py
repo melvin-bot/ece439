Fixed vecangle x0 sign

mobrob/src/closed_loop_path_follower.py
@@ -64,10 +64,10 @@ path_is_complete = False
 
 ####    CODE HERE: Create two publishers as described
 # Create the publisher. Name the topic "wheel_speeds_desired", with message type "ME439WheelSpeeds"
-pub_speeds = rospy.Publisher('/wheel_speeds_desired', ME439WheelSpeeds, queue_size = 1)
+pub_speeds = rospy.Publisher('/wheel_speeds_desired', ME439WheelSpeeds, queue_size=1)
 
 # Create the publisher for "segment_complete". Name the topic "segment_complete", with message type "Bool"
-pub_segment_complete = rospy.Publisher('segment_complete', Bool, queue_size = 1)
+pub_segment_complete = rospy.Publisher('/segment_complete', Bool, queue_size=1)
 ####    CODE END
 
 # Set up the node and its subscriptions, and keep it alive.
@@ -224,7 +224,7 @@ def path_follow(pose_msg_in):
     xdot_local_desired = np.min([np.abs(xdot_local_desired),abs(Vmax)])*np.sign(xdot_local_desired)
     
     # Next set the desired theta_local 
-    theta_local_desired = np.arcsin((-xdot_local_desired)/Vmax)   # Use formula from Lecture
+    theta_local_desired = np.arcsin(-xdot_local_desired/Vmax)   # Use formula from Lecture
             
     ## Next SET SPEED OF ROBOT CENTER. 
     ## G. Cook 2011 says just use constant speed all the time,
@@ -245,7 +245,7 @@ def path_follow(pose_msg_in):
     
     # Finally set the desired angular rate
     estimated_theta_local_error = m439rbt.fix_angle_pi_to_neg_pi(theta_local_desired - estimated_theta_local)
-    omega = gamma * estimated_theta_local_error + path_segment_curvature / (1 + path_segment_curvature * estimated_x_local) * Vc * np.cos(estimated_theta_local)
+    omega = gamma*estimated_theta_local_error + Vc*(path_segment_curvature/(1.0+path_segment_curvature*estimated_x_local))*np.cos(estimated_theta_local)
 ####    CODE END    
     
     # Finally, use the "robot" object created elsewhere (member of the me439_mobile_robot_xx class) to translate omega and Vc into wheel speeds

mobrob/src/me439_mobile_robot_class_v02.py
@@ -206,10 +206,11 @@ class robot:
         yf = float(yf)
         
 ####    CODE HERE: 
-        vec = np.array([xf-x0,yf-y0]) # vector from [x0,y0] to [xf,yf], stored as a numpy array (np.array())
-        dist = np.sqrt((xf-x0) ** 2 + (yf-y0) ** 2)    # distance from [x0,y0] to [xf,yf], a Scalar
-        vecangle = np.arctan2(vec[0], vec[1])       # Vector angle: angle of the vector directly to the end point. A Scalar, compute with numpy's "arctan2"
+        vec = np.array([xf-x0,yf-y0])           # vector from [x0,y0] to [xf,yf], stored as a numpy array (np.array())
+        dist = np.linalg.norm(vec)              # distance from [x0,y0] to [xf,yf], a Scalar
+        vecangle = np.arctan2(-vec[0], vec[1])  # Vector angle: angle of the vector directly to the end point. A Scalar, compute with numpy's "arctan2"
 ####    CODE END
+
         return [x0, y0, vecangle, np.inf, dist]    # Return
 
 
@@ -225,8 +226,8 @@ class robot:
         R = float(R)
 
 ####    CODE HERE:  Edit everything that has 0.0 or [] assigned to give it the right values!       
-        vec = np.array([xf-x0,yf-y0]) # vector from [x0,y0] to [xf,yf], stored as a numpy array (np.array())
-        dist = np.sqrt((xf-x0) ** 2 + (yf-y0) ** 2)     # distance from [x0,y0] to [xf,yf], a Scalar
+        vec = np.array([xf-x0,yf-y0])   # vector from [x0,y0] to [xf,yf], stored as a numpy array (np.array())
+        dist = np.linalg.norm(vec)      # distance from [x0,y0] to [xf,yf], a Scalar
         
         # Code to determine how a circular path of Radius R can get from start to finish of this path segment.  
         if dist > 2.0*np.abs(R):                                # If the endpoint is farther away than the diameter of the circle, an arc cannot get there and you must draw a line instead. 
@@ -235,7 +236,7 @@ class robot:
             arcangle = 2.0*np.arcsin((dist/2.0)/R )             # Smallest arc angle is swept if going the "short way" around the circle. The distance from start to finish is the "chord length" and relates to the included angle as shown. 
             if way.lower() == 'long':
                 arcangle = 2*np.pi*np.sign(arcangle) - arcangle # arc angle swept if going the "long way": a full circle is 2*pi, so the long way is 2*pi minus the short way. Note the signs. 
-            vecangle = np.arctan2(vec[0], vec[1])               # Vector angle: angle of the vector directly from the start point to the end point.
+            vecangle = np.arctan2(-vec[0], vec[1])              # Vector angle: angle of the vector directly from the start point to the end point.
             initangle = vecangle - arcangle/2.0                 # Initial tangent angle of the arc to be followed. Deviates from the direct straignt line by half the included angle of the arc. 
             initangle = fix_angle_pi_to_neg_pi(initangle)       # Could have gone past pi with the addition. Unwrap to make sure it's within +/- pi. 
             arclength = arcangle * R                            # arc length is also needed. Enter it based on Radius and Angle. 
@@ -283,13 +284,13 @@ def convert_stage_settings_to_path_specs(xytheta_init, stages, wheel_width):
         robot_omega = (right_vel - left_vel) / wheel_width   # Determine the angular velocity of the robot
         # Then compute the length and angle of the path it follows
         path_length = robot_vel * delta_time    # Determine the path segment's length (e.g. use speed and time)
-        delta_S_left = (left_vel * delta_time) - xy_beg[0]
-        delta_S_right = (right_vel * delta_time) - xy_beg[1]
-        delta_theta = (delta_S_right - delta_S_left) / wheel_width     # Determine the path segment's change in angle 
+        # delta_S_left = (left_vel * delta_time) - xy_beg[0]
+        # delta_S_right = (right_vel * delta_time) - xy_beg[1]
+        delta_theta = delta_time * robot_omega  # Determine the path segment's change in angle 
 
         # Determine the radius of curvature of the path segment
         if robot_omega == 0:    
-            path_radius = np.inf()    # If there's Zero angular velocity, what does that mean about the Radius? 
+            path_radius = np.inf    # If there's Zero angular velocity, what does that mean about the Radius? 
         else:
             path_radius = robot_vel / robot_omega    # Otherwise what's the standard relationship among Radius, velocity and angular velocity? 
 ####    CODE END     
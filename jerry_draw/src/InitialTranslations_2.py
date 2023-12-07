c70d85d4dfc49e7f7981476346aeb9a660b35276
SpookySquidward <willrkramer@gmail.com>
2023-12-07 14:36:00 -0600

    Wrap world-position translation in function

--- jerry_draw/src/InitialTranslations_2.py
+++ jerry_draw/src/InitialTranslations_2.py
@@ -1,31 +1,39 @@
 import numpy as np
+from jerry_draw.msg import aruco_marker_position
 
+# Get this from the YAML!
 camera_angle = np.deg2rad(7)
 
-Xa = 0.2
-Ya = 0.3
-Za = 0.9
+def aruco_marker_world_pos(camera_pos: aruco_marker_position):
 
-C_rotate_x_pitch = np.array([[1, 0, 0, Xa],
-                       [0, np.cos(camera_angle), -np.sin(camera_angle), Ya],
-                       [0, np.sin(camera_angle), np.cos(camera_angle), Za],
-                       [0, 0, 0, 1]])
-C_rotate_x = np.array([[1, 0, 0, 0],
-                       [0, np.cos(np.pi/2), -np.sin(np.pi/2), 0],
-                       [0, np.sin(np.pi/2), np.cos(np.pi/2), 0],
-                       [0, 0, 0,1]])
-C_rotate_z = np.array([[np.cos(np.pi/2), -np.sin(np.pi/2), 0, 0],
-                       [np.sin(np.pi/2), np.cos(np.pi/2), 0, 0],
-                       [0, 0, 1, 0],
-                       [0, 0, 0, 1]])
-T_c_j = np.array([[0.085],
-                 [0],
-                 [-0.295],
-                 [1]]
-                 )
+    C_rotate_x_pitch = np.array([[1, 0, 0, camera_pos.x],
+                        [0, np.cos(camera_angle), -np.sin(camera_angle), camera_pos.y],
+                        [0, np.sin(camera_angle), np.cos(camera_angle), camera_pos.z],
+                        [0, 0, 0, 1]])
+    C_rotate_x = np.array([[1, 0, 0, 0],
+                        [0, np.cos(np.pi/2), -np.sin(np.pi/2), 0],
+                        [0, np.sin(np.pi/2), np.cos(np.pi/2), 0],
+                        [0, 0, 0,1]])
+    C_rotate_z = np.array([[np.cos(np.pi/2), -np.sin(np.pi/2), 0, 0],
+                        [np.sin(np.pi/2), np.cos(np.pi/2), 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
+    T_c_j = np.array([[0.085],
+                    [0],
+                    [-0.295],
+                    [1]]
+                    )
 
-M1 = np.matmul(C_rotate_x_pitch, C_rotate_x)
-M2 = np.matmul(M1, C_rotate_z)
-M3 = np.matmul(M2, T_c_j)
+    M1 = np.matmul(C_rotate_x_pitch, C_rotate_x)
+    M2 = np.matmul(M1, C_rotate_z)
+    M3 = np.matmul(M2, T_c_j)
 
-print("Aruco in World frame:", "\n", M3)
+    return M3[:3, 0]
+
+
+if __name__ == '__main__':
+    test_aruco_position = aruco_marker_position
+    test_aruco_position.x = 0.2
+    test_aruco_position.y = 0.3
+    test_aruco_position.z = 0.9
+    print("Aruco in World frame:", "\n", aruco_marker_world_pos(test_aruco_position))

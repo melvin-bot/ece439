1c78f17d9ebc8a95eae3b0464c4898552d83af2a
Jed <jedediah.pulley@gmail.com>
2023-12-08 15:53:15 -0600

    functionalize kinematics

--- jerry_draw/src/inverse_kinematics.py
+++ jerry_draw/src/inverse_kinematics.py
@@ -2,10 +2,6 @@ import numpy as np
 import rospy
 
 target = np.array([0.36,-0.28, 0.28])
-target_x = target[0]
-target_y = target[1]
-target_z = target[2]
-
 
 l1 = rospy.get_param("/frame_offset_23")[0]
 l3 = rospy.get_param("/frame_offset_23")[0]
@@ -21,31 +17,34 @@ dz_gripper = rospy.get_param("/endpoint_offset_in_frame_6")[2]
 
 l2 = np.sqrt(-dz_gripper**2 + (dx_gripper + d2 + l3)**2)
 
-# find beta 2 prime
-delta_r = np.sqrt(target_x**2 + target_y**2) - x1
-delta_z = target_z - z1
+def pen_inverse_kinematics(target):
+    target_x = target[0]
+    target_y = target[1]
+    target_z = target[2]
 
-d = np.sqrt(delta_r**2 + delta_z**2)
+    alpha = np.arctan2(target_y / target_x)
 
-delta = np.arccos((l1**2 + l2**2 - d**2) / (2 * l1 * l2))
-beta2_prime = np.pi - delta
+    # find beta 2 prime
+    delta_r = np.sqrt(target_x**2 + target_y**2) - x1
+    delta_z = target_z - z1
 
-# find beta 1
-psi = np.arctan2(delta_z/delta_r)
-phi = np.arccos((l1**2 + d**2 - l2**2) / 2 * l1 * d)
+    d = np.sqrt(delta_r**2 + delta_z**2)
 
-beta1 = psi + phi
+    delta = np.arccos((l1**2 + l2**2 - d**2) / (2 * l1 * l2))
+    beta2_prime = np.pi - delta
 
-# find beta 3
-beta3 = (np.pi / 2) - np.arctan2(l5/l4)
+    # find beta 1
+    psi = np.arctan2(delta_z/delta_r)
+    phi = np.arccos((l1**2 + d**2 - l2**2) / 2 * l1 * d)
 
-# find beta_wrist
-beta_wrist = -beta3
+    beta1 = psi + phi
 
-# find beta2 error
-beta2_err = np.arctan2(-dz_gripper / (dx_gripper + d2 + l3))
+    # find beta 3
+    beta3 = (np.pi / 2) - np.arctan2(l5/l4)
 
-beta2 = beta2_prime - beta2_err
+    # find beta2 error
+    beta2_err = np.arctan2(-dz_gripper / (dx_gripper + d2 + l3))
 
-if __name__ == '__main__':
-    print("Beta 1: " + beta1 + "\nBeta2: " + beta2 + "\nBeta3: " + beta3)
+    beta2 = beta2_prime - beta2_err
+
+    return([alpha, beta1, beta2, beta3])

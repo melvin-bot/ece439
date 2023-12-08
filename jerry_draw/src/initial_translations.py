Fix world pos rotation matrix

jerry_draw/src/initial_translations.py
@@ -1,45 +1,35 @@
 import numpy as np
 # import rospy
-# from jerry_draw.msg import aruco_marker_position
+from jerry_draw.msg import aruco_marker_position
 
-# Get this from the YAML!
-camera_angle = np.deg2rad(7)
 # camera_angle = rospy.get_param("/camera_pitch")
 
-def aruco_marker_world_pos(camera_pos, camera_angle):
+def aruco_marker_world_pos(marker_pos_camera, camera_angle, camera_position):
 
-    C_rotate_x_pitch = np.array([[1, 0, 0, camera_pos.x],
-                        [0, np.cos(camera_angle), -np.sin(camera_angle), camera_pos.y],
-                        [0, np.sin(camera_angle), np.cos(camera_angle), camera_pos.z],
-                        [0, 0, 0, 1]])
-    C_rotate_x = np.array([[1, 0, 0, 0],
-                        [0, np.cos(np.pi/2), -np.sin(np.pi/2), 0],
-                        [0, np.sin(np.pi/2), np.cos(np.pi/2), 0],
-                        [0, 0, 0,1]])
-    C_rotate_z = np.array([[np.cos(np.pi/2), -np.sin(np.pi/2), 0, 0],
-                        [np.sin(np.pi/2), np.cos(np.pi/2), 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 0, 0, 1]])
-    T_c_j = np.array([[0.085],
-                    [0],
-                    [-0.295],
-                    [1]]
-                    )
+    # The camera coordinates of the tag are weird; use the standard x-forward notation we've used previously
+    pos_tag = np.array([[marker_pos_camera.z], [-marker_pos_camera.x], [-marker_pos_camera.y], [1]])
 
-    M1 = np.matmul(C_rotate_x_pitch, C_rotate_x)
-    M2 = np.matmul(M1, C_rotate_z)
-    M3 = np.matmul(M2, T_c_j)
+    # Translate from camera-space to world-space with a transform matrix
+    camera_position = camera_position.reshape((-1))
+    transform_camera_world = np.array(
+        [[np.cos(camera_angle),   0,  np.sin(camera_angle),  camera_position[0]],
+         [0,                      1,  0,                     camera_position[1]],
+         [-np.sin(camera_angle),  0,  np.cos(camera_angle),  camera_position[2]],
+         [0,                      0,  0,                     1]]
+    )
+    pos_world = transform_camera_world @ pos_tag
 
-    return M3[:3, 0]
+    # Pull out the x, y, z components of pos_world and return them
+    return pos_world[:3, 0]
 
 
 if __name__ == '__main__':
-    class dummy:
-        def __init__(self):
-            pass
+    test_aruco_position = aruco_marker_position()
+    test_aruco_position.x = -0.05
+    test_aruco_position.y = 0.0
+    test_aruco_position.z = 0.46
 
-    test_aruco_position = dummy()
-    test_aruco_position.x = -0.0513
-    test_aruco_position.y = 0.0034
-    test_aruco_position.z = 0.4645
-    print("Aruco in World frame:", "\n", aruco_marker_world_pos(test_aruco_position, camera_angle))
+    test_camera_angle = np.deg2rad(7)
+    test_camera_position = np.array([-0.085, 0, 0.295])
+
+    print("Aruco in World frame:", "\n", aruco_marker_world_pos(test_aruco_position, test_camera_angle, test_camera_position))
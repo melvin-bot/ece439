ab0e0982786190079c83109997c3974c56c67e3d
Jed <jedediah.pulley@gmail.com>
2023-12-08 15:44:33 -0600

    inverse kinematics

---
+++ jerry_draw/src/inverse_kinematics.py
@@ -0,0 +1,51 @@
+import numpy as np
+import rospy
+
+target = np.array([0.36,-0.28, 0.28])
+target_x = target[0]
+target_y = target[1]
+target_z = target[2]
+
+
+l1 = rospy.get_param("/frame_offset_23")[0]
+l3 = rospy.get_param("/frame_offset_23")[0]
+l4 = rospy.get_param("/frame_offset_34")[0]
+l5 = rospy.get_param("/frame_offset_34")[2]
+x1 = rospy.get_param("/frame_offset_12")[0]
+z1 = rospy.get_param("/frame_offset_01")[2]
+
+d2 = np.sqrt(l4**2 + l5**2)
+
+dx_gripper = rospy.get_param("/endpoint_offset_in_frame_6")[0]
+dz_gripper = rospy.get_param("/endpoint_offset_in_frame_6")[2]
+
+l2 = np.sqrt(-dz_gripper**2 + (dx_gripper + d2 + l3)**2)
+
+# find beta 2 prime
+delta_r = np.sqrt(target_x**2 + target_y**2) - x1
+delta_z = target_z - z1
+
+d = np.sqrt(delta_r**2 + delta_z**2)
+
+delta = np.arccos((l1**2 + l2**2 - d**2) / (2 * l1 * l2))
+beta2_prime = np.pi - delta
+
+# find beta 1
+psi = np.arctan2(delta_z/delta_r)
+phi = np.arccos((l1**2 + d**2 - l2**2) / 2 * l1 * d)
+
+beta1 = psi + phi
+
+# find beta 3
+beta3 = (np.pi / 2) - np.arctan2(l5/l4)
+
+# find beta_wrist
+beta_wrist = -beta3
+
+# find beta2 error
+beta2_err = np.arctan2(-dz_gripper / (dx_gripper + d2 + l3))
+
+beta2 = beta2_prime - beta2_err
+
+if __name__ == '__main__':
+    print("Beta 1: " + beta1 + "\nBeta2: " + beta2 + "\nBeta3: " + beta3)
